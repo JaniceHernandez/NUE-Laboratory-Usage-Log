78ea758fe28b82a241f3b38a7a5c8d5fed4f8e0d
Janice V. Hernandez <janice.hernandez@neu.edu.ph>
2026-02-28 06:42:49 +0000

    Can you use this firestore??
    // Import the functions you need from the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "api-key-placeholder",
-  authDomain: "project-id-placeholder.firebaseapp.com",
-  projectId: "project-id-placeholder",
-  storageBucket: "project-id-placeholder.firebasestorage.app",
-  messagingSenderId: "sender-id-placeholder",
-  appId: "app-id-placeholder"
+  apiKey: "AIzaSyBlh4WAAorypj28KXsBSzQ3ho19h5vUuDU",
+  authDomain: "studio-20404588-b672e.firebaseapp.com",
+  projectId: "studio-20404588-b672e",
+  storageBucket: "studio-20404588-b672e.firebasestorage.app",
+  messagingSenderId: "357982471157",
+  appId: "1:357982471157:web:f120f90e1b21c61a8d96bc"
 };
